f6bff152752126b77eccc15ad3609a22f102c2f4
Terry Matthew Poulin <BigBoss1964@gmail.com>
2009-04-16 09:14:20 +0000

    fixed quoting in several places

--- init.sh
+++ init.sh
@@ -4,11 +4,19 @@
 #
 #
 
+# setup a tmpdir and umask for this file
+INIT_TMPDIR="${TMPDIR:-/tmp}"
+umask 077
 
 #
 # Force parsing of our current X resources file
 #
 xrdb -quiet ~/.Xresources
+
+#
+# Fancy shadows and things
+#
+xcompmgr -CfFns &
 
 #
 # launch a D-Bus channel for this session
@@ -17,24 +25,40 @@ which dbus-launch >/dev/null && if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
     eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
+# download dir for MPlayer plugin
+mkdir "${INIT_TMPDIR}/mplayer"
 
-~/sh/plsetbg &
-
-~/sh/startup-sound.sh &
-bbkeys &
-
-fbpanel -p tray &
-fbpanel -p pager &
-
-gkrellm &
-
+#
+# Eexecute my primary user interface
+#
 urxvt -title ${USER}@`hostname -s` -e screen -U &
 
+
+# launchwall paper changer
+mkdir "${INIT_TMPDIR}/plsetbg"
+~/sh/plsetbg -D "${INIT_TMPDIR}/plsetbg" &
+
+#
+# Window dressings (docs, panels, applets, etc)
+#
+fbpanel -p tray &
+fbpanel -p pager &
+gkrellm &
+
+#
+# Only launch these if we have an internet connection
+#
 if [ ! -z "`ifconfig | grep -v lo0 | grep UP`" ]; then
     pidgin &
     #psi &
-    #xchat --minimize=2 &
 fi
 
+#
+# Last minute stuffs
+#
 ~/sh/xkillname xconsole &
+~/sh/startup-sound.sh &
+transset-df -n "Terry@dixie" .9
+
+# start the big kahuna
 fvwm

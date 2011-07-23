e9b3198fedc64f4b5f876ff2376c1feb8a741fd5
Terry M. Poulin <BigBoss1964@gmail.com>
2011-07-23 20:15:09 +0000

    Ditch GNOME; use fbpanel + dmenu + gmrun in place of GNOME portions.
    N.b. on most distro including ubuntu, this will need at least four
    packages:
    
    	0. xmonad
    	1. fbpanel
    	2. dmenu
    	3. gmrun

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,25 @@
 import XMonad
-import XMonad.Config.Gnome
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
 
---main = xmonad $ defaultConfig {
-main = xmonad $ gnomeConfig {
-        --modMask = mod4Mask,
+-- uncomment this and set logHook to myLogHook if you want to make inactive windows translucent
+--
+--import XMonad.Hooks.FadeInactive
+--myLogHook :: X ()
+--myLogHook = fadeInactiveLogHook fadeAmount
+--        where fadeAmount = 0x99999999 -- 60% rgba
+
+main = xmonad $ defaultConfig {
+        manageHook = manageDocks <+> manageHook defaultConfig,
+        layoutHook = avoidStruts $ layoutHook defaultConfig,
         terminal = "/home/terry/sw/sh.local/myterm"
     }
     `additionalKeysP` [
-        ("M-S-q", spawn "gnome-session-save --gui --logout-dialog"),
+        -- use this when running in gnome
+        --("M-S-q", spawn "gnome-session-save --gui --logout-dialog"),
+
         --("M-p", spawn "gmrun"),
+        --("M-S-p", spawn "dmenu"),
+
         ("M-S-w", spawn "google-chrome")
     ]

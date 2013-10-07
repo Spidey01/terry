83e13ad124b8be15bb224528764f997f60e345b9
Terry M. Poulin <BigBoss1964@gmail.com>
2013-10-07 17:27:15 +0000

    Fix full screen (e.g. in Chrome) in Xmonad config.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,21 +1,43 @@
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import qualified XMonad.StackSet as W;
 import XMonad.Util.EZConfig
 
 -- uncomment this and set logHook to myLogHook if you want to make inactive windows translucent
 --
---import XMonad.Hooks.FadeInactive
---myLogHook :: X ()
---myLogHook = fadeInactiveLogHook fadeAmount
---        where fadeAmount = 0x99999999 -- 60% rgba
+-- import XMonad.Hooks.FadeInactive
+-- myLogHook :: X ()
+-- myLogHook = fadeInactiveLogHook fadeAmount
+--         where fadeAmount = 0x999999999 -- 60% rgba
 
-main = xmonad $ ewmh defaultConfig {
-        modMask = mod4Mask,
-        --handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook,
-        manageHook = manageDocks <+> manageHook defaultConfig,
-        layoutHook = avoidStruts $ layoutHook defaultConfig,
-        terminal = "xterm"
+
+myManageHooks = composeAll
+    [
+    -- stuff for full screen as recommended by the wiki. Doesn't seem to be needed.
+    isFullscreen --> (doF W.focusDown <+> doFullFloat) 
+    -- for keeping space for a panel. See layoutHook with avoidStruts.
+    , manageDocks
+    , manageHook defaultConfig
+    ]
+
+-- smartBorders: don't stuff boards on e.g. full screen window.
+-- avoidStruts: for making the panel work.
+myLayoutHooks = smartBorders (avoidStruts $ layoutHook defaultConfig)
+
+-- Fixes some full screen apps, namely Google Chrome.
+myHandleEventHooks = handleEventHook defaultConfig <+> fullscreenEventHook
+
+main = xmonad $ ewmh defaultConfig { modMask = mod4Mask
+        , handleEventHook = myHandleEventHooks
+        , manageHook = myManageHooks 
+        , layoutHook = myLayoutHooks
+        -- , logHook = myLogHook
+
+        -- set terminal for mond-shift-enter
+        , terminal = "xterm"
     }
     `additionalKeysP` [
         -- use this when running in gnome
@@ -27,7 +49,16 @@ main = xmonad $ ewmh defaultConfig {
         -- Retired..
         -- ("M-S-o", spawn "opera"),
 
-
-	-- triggle screen saver. "Lock" when set as I normally have it.
-	("M-S-l", spawn "xscreensaver-command -activate")
+        -- triggle screen saver. "Lock" when set as I normally have it.
+        ("M-S-l", spawn "xscreensaver-command -activate")
     ]
+
+-- API notes
+--      XMonad.ManageHook :: http://xmonad.org/xmonad-docs/xmonad/XMonad-ManageHook.html
+--          <+> Monoid m => m -> m -> m
+--              compose two Manage hook from right to left.
+--          composeAll :: Monoid m => [m] -> m
+--              compose the list of ManageHook's
+--          --> :: (Monad m, Monoid a) => m Bool -> m a -> m a
+--              p --> x. If p returns True then execute the ManageHook
+--      XMonad.Hooks.ManageDocks :: http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Hooks-ManageDocks.html

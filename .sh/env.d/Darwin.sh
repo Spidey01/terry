68060fcc78db67a529746885217f4d11eaaeaaee
Terry M. Poulin <BigBoss1964@gmail.com>
2023-02-10 22:04:33 +0000

    sh: ditto MANPATH.

--- .sh/env.d/Darwin.sh
+++ .sh/env.d/Darwin.sh
@@ -1,5 +1,5 @@
 
 if [ -d "/Applications/Wireshark.app" ]; then
     ensure_path -ae "/Applications/Wireshark.app/Contents/MacOS/"
-    MANPATH="/Applications/Wireshark.app/Contents/Resources/share/man:${MANPATH}"
+    prepend_pathlike MANPATH "/Applications/Wireshark.app/Contents/Resources/share/man"
 fi

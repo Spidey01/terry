e73d7ab56bd511f53eb9a2ec14a2c67b6881910a
Terry M. Poulin <BigBoss1964@gmail.com>
2023-02-10 21:18:52 +0000

    sh: update python environment.

--- .sh/env.d/python.sh
+++ .sh/env.d/python.sh
@@ -1,9 +1,9 @@
-
-# Python stuff
 #
-if in_path -e python; then
-    PYTHONSTARTUP="${HOME}/.pythonrc"
-    [ ! -f "$PYTHONSTARTUP" ] && touch "$PYTHONSTARTUP"
-    PYTHONPATH="${HOME}/sw/lib/python`get_python_major_version`/site-packages:$PYTHONPATH"
-    export PYTHONSTARTUP PYTHONPATH
-fi
+# Python stuff.
+#
+
+PYTHONSTARTUP="${HOME}/.pythonrc"
+# Assumes get_python_major_version is silent on failure.
+append_pathlike PYTHONPATH "${HOME}/sw/lib/python`get_python_major_version`/site-packages"
+
+export PYTHONSTARTUP PYTHONPATH
